e414e9b26ab6aeeda953507ace7f815a0c4b2532
Dalex994 <dalex.laville@protonmail.com>
2023-05-20 17:36:22 +0200

    F*cking UserControl fixed

--- Askilian Launcher WPF/UserControls/PopupUserControl.xaml.cs
+++ Askilian Launcher WPF/UserControls/PopupUserControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Askilian_Launcher
+namespace Askilian_Launcher.UserControls
 {
     /// <summary>
     /// Logique d'interaction pour PopupUserControl.xaml
